9f88174eaadc51742099e65b35cf96d46da2d13f
Shadha-K <kshadha@gmail.com>
2024-02-23 13:52:49 -0600

    Added home page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import { Link } from 'react-router-dom';
 import './Components/Navigation/Navigation'; // Import the navigation CSS file
 import './Components/Navigation/Navigation.css';
 
+import HomePage from './Components/HomePage/HomePage';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
 import Registration from './Components/Registration/Registration';
 import UserProfile from './Components/UserProfile/UserProfile';
 import UserFuelRequest from './Components/UserFuelRequest/UserFuelRequest'
-import FuelRequest from './Components/FuelRequest/script';
 import EditProfile from './Components/EditProfile/EditProfile';
 
 function Navigation() {
   return (
     <nav>
       <ul>
+        <li><Link to="/homepage">HomePage</Link></li>
         <li><Link to="/login-signup">Login/Signup</Link></li>
         <li><Link to="/registration">Registration</Link></li>
         <li><Link to="/profile">UserProfile</Link></li>
@@ -31,6 +32,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/homepage" element={<HomePage />} />
         <Route path="/login-signup" element={<LoginSignup />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/profile" element={<UserProfile />} />
